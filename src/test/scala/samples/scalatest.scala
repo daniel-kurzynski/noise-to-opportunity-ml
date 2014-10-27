8f948f263cf0400371fe2f323a65021a248f1788
Stefan Bunk <stefan.bunk@googlemail.com>
2014-10-27 17:08:39 +0100

    Scaffold application.

---
+++ src/test/scala/samples/scalatest.scala
@@ -0,0 +1,17 @@
+package samples
+
+import scala.collection.mutable.Stack
+import org.scalatest.Assertions
+import org.junit.Test
+
+import org.scalatest.FunSuite
+import org.scalatest.matchers.ShouldMatchers
+
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
+
+@RunWith(classOf[JUnitRunner])
+class ListSuite extends FunSuite {
+	test("An empty list should be empty") {
+	}
+}
