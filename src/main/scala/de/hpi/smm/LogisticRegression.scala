01fe32f37c40767865ca55319e688d2fea53a63c
Stefan Bunk <stefan.bunk@googlemail.com>
2014-10-27 17:20:57 +0100

    Add mahout dependency.

---
+++ src/main/scala/de/hpi/smm/LogisticRegression.scala
@@ -0,0 +1,13 @@
+package de.hpi.smm
+
+import org.apache.mahout.vectorizer.encoders.FeatureVectorEncoder
+
+class LogisticRegression {
+
+	def foo(): Unit = {
+		val features = new FeatureVectorEncoder("FOO")
+
+
+	}
+
+}
