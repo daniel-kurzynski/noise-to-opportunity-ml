74570e100657008a52f7ae26b9155a1b1fd7f246
Dimitri Korsch <korschdima@yahoo.de>
2014-11-14 13:46:51 +0100

    added some classifiers

--- demo.py
+++ demo.py
@@ -62,68 +62,86 @@ def process(filename, line_extractor):
   y_test = np.array(content["target"])
 
   results = []
+  data = [X_train,X_test,y_train,y_test]
   for classifier, name in (
         (RidgeClassifier(tol=1e-2, solver="lsqr"), "Ridge Classifier"),
         (Perceptron(n_iter=50), "Perceptron"),
         (PassiveAggressiveClassifier(n_iter=50), "Passive-Aggressive"),
         (KNeighborsClassifier(n_neighbors=10), "kNN")):
 
-  	results.append(benchmark(classifier,X_train,X_test,y_train,y_test))
+    results.append(benchmark(classifier, *data))
+  for penalty in ["l2", "l1"]:
+    # Train Liblinear model
+    results.append(benchmark(
+      LinearSVC(loss='l2', penalty=penalty, dual=False, tol=1e-3), *data))
+    # Train SGD model
+    results.append(benchmark(SGDClassifier(alpha=.0001, n_iter=50,
+                                           penalty=penalty), *data))
+  # Train SGD with Elastic Net penalty
+  results.append(benchmark(SGDClassifier(alpha=.0001, n_iter=50,
+                                         penalty="elasticnet"), *data))
+
+  # Train NearestCentroid without threshold
+  results.append(benchmark(NearestCentroid(), *data))
+
+  # Train sparse Naive Bayes classifiers
+  results.append(benchmark(MultinomialNB(alpha=.01), *data))
+  results.append(benchmark(BernoulliNB(alpha=.01), *data))
 
   print_result(results)
   visualize_result(results)
 
 def print_result(result):
-	for classifier, train_time, test_time, score, confusion_matrix in result:
-		print classifier
-		print("train time: %0.13fs" % train_time)
-		print("test time:  %0.13fs" % test_time)
-		print("f1-score:   %0.13f" % score)
-		print confusion_matrix
+  for classifier, train_time, test_time, score, confusion_matrix in result:
+    print classifier
+    print("train time: %0.13fs" % train_time)
+    print("test time:  %0.13fs" % test_time)
+    print("f1-score:   %0.13f" % score)
+    print confusion_matrix
 
 def benchmark(classifier,X_train,X_test,y_train,y_test):
-	t0 = time()
-	classifier.fit(X_train, y_train)
-	train_time = time() - t0
+  t0 = time()
+  classifier.fit(X_train, y_train)
+  train_time = time() - t0
 
-	print train_time
+  print train_time
 
-	t0 = time()
-	pred = classifier.predict(X_test)
-	test_time = time() - t0
+  t0 = time()
+  pred = classifier.predict(X_test)
+  test_time = time() - t0
 
-	score = metrics.f1_score(y_test, pred)
+  score = metrics.f1_score(y_test, pred)
 
-	confusion_matrix = metrics.confusion_matrix(y_test, pred)
+  confusion_matrix = metrics.confusion_matrix(y_test, pred)
 
-	return classifier.__class__.__name__,train_time, test_time, score,confusion_matrix
+  return classifier.__class__.__name__,train_time, test_time, score,confusion_matrix
 
 def visualize_result(results):
-	indices = np.arange(len(results))
+  indices = np.arange(len(results))
 
-	results = [[x[i] for x in results] for i in range(4)]
+  results = [[x[i] for x in results] for i in range(4)]
 
-	classifier_names, training_time, test_time, score = results
-	if(np.max(training_time)>0):
-		training_time = np.array(training_time) / np.max(training_time)
-	if(np.max(test_time)>0):
-		test_time = np.array(test_time) / np.max(test_time)
+  classifier_names, training_time, test_time, score = results
+  if(np.max(training_time)>0):
+    training_time = np.array(training_time) / np.max(training_time)
+  if(np.max(test_time)>0):
+    test_time = np.array(test_time) / np.max(test_time)
 
-	plt.figure(figsize=(12, 8))
-	plt.title("Score")
-	plt.barh(indices, score, .2, label="score", color='r')
-	plt.barh(indices + .3, training_time, .2, label="training time", color='g')
-	plt.barh(indices + .6, test_time, .2, label="test time", color='b')
-	plt.yticks(())
-	plt.legend(loc='best')
-	plt.subplots_adjust(left=.25)
-	plt.subplots_adjust(top=.95)
-	plt.subplots_adjust(bottom=.05)
+  plt.figure(figsize=(12, 8))
+  plt.title("Score")
+  plt.barh(indices, score, .2, label="score", color='r')
+  plt.barh(indices + .3, training_time, .2, label="training time", color='g')
+  plt.barh(indices + .6, test_time, .2, label="test time", color='b')
+  plt.yticks(())
+  plt.legend(loc='best')
+  plt.subplots_adjust(left=.25)
+  plt.subplots_adjust(top=.95)
+  plt.subplots_adjust(bottom=.05)
 
-	for i, c in zip(indices, classifier_names):
-	    plt.text(-.3, i, c)
+  for i, c in zip(indices, classifier_names):
+      plt.text(-.3, i, c)
 
-	plt.show()
+  plt.show()
 
 print "Brochures"
 print "="*50
@@ -131,7 +149,7 @@ process("data/brochures.csv", brochure_exractor)
 print "="*50
 
 
-print "LinkedIn Posts"
-print "="*50
-process("data/linked_in_posts.csv", linked_in_extractor)
-print "="*50
+# print "LinkedIn Posts"
+# print "="*50
+# process("data/linked_in_posts.csv", linked_in_extractor)
+# print "="*50
