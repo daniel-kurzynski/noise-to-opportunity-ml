2bb1b76e2bb6fc722e72a0b93b668b0dfd6ea39d
Daniel Kurzynski <daniel.kurzynski@student.hpi.uni-potsdam.de>
2014-11-14 11:49:07 +0100

    mlib test

---
+++ mahout_java/src/main/scala/de/hpi/smm/ClassificationMLLib.scala
@@ -0,0 +1,10 @@
+package de.hpi.smm
+
+import weka.core.converters.CSVLoader
+
+
+object ClassificationMLLib {
+	def main(args: Array[String]): Unit = {
+		//val sparkContext = new SparkContext("local[4]", "naivebayes")
+	}
+}
