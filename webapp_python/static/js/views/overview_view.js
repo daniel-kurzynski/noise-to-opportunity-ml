a99695672e0bf2b06f2b6bc39be49cdf7b8aaadf
Daniel Kurzynski <daniel.kurzynski@student.hpi.uni-potsdam.de>
2014-11-26 17:24:08 +0100

    overview view shows certain/uncertain posts

--- webapp_python/static/js/views/overview_view.js
+++ webapp_python/static/js/views/overview_view.js
@@ -5,12 +5,13 @@ define([
 	'text!views/overview_view.html',
 	'text!views/tagged_posts.html',
 	'text!views/predicted_posts.html'
-	], function($, _, Backbone, OverviewViewTemplate, TaggedPostsTemplate) {
+	], function($, _, Backbone, OverviewViewTemplate, TaggedPostsTemplate, PredictedPostsTemplate) {
 
 		return Backbone.View.extend({
 
 			template: _.template(OverviewViewTemplate),
 			taggedPostsTemplate: _.template(TaggedPostsTemplate),
+			predictedPostsTemplate: _.template(PredictedPostsTemplate),
 
 			events: {
 
@@ -24,6 +25,8 @@ define([
 			render: function() {
 				this.$el.html(this.template({}));
 				this.loadTaggedPosts();
+				this.loadCertainPosts();
+				this.loadUncertainPosts();
 			},
 
 			loadTaggedPosts:function(){
@@ -31,6 +34,22 @@ define([
 				$.get('/all_tagged_posts',function(result){
 					posts = JSON.parse(result).posts;
 					self.$el.find('#tagged-posts').html(self.taggedPostsTemplate({posts:posts}));
+				});
+			},
+
+			loadCertainPosts:function(){
+				var self = this;
+				$.get('/certain_posts',function(result){
+					posts = JSON.parse(result).posts;
+					self.$el.find('#certain-posts').html(self.predictedPostsTemplate({posts:posts}));
+				});
+			},
+
+			loadUncertainPosts:function(){
+				var self = this;
+				$.get('/uncertain_posts',function(result){
+					posts = JSON.parse(result).posts;
+					self.$el.find('#uncertain-posts').html(self.predictedPostsTemplate({posts:posts}));
 				});
 			}
 
