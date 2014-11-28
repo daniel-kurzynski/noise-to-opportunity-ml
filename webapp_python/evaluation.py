c47f522968dc799df46319618a8b429e2a4bf26b
Stefan Bunk <stefan.bunk@googlemail.com>
2014-11-28 15:04:06 +0100

    Confusion matrix visualization.

--- webapp_python/evaluation.py
+++ webapp_python/evaluation.py
@@ -1,7 +1,15 @@
 from active_learner import active_learner
+import matplotlib.pyplot as plt
 
 if __name__ == "__main__":
 	learner = active_learner()
 	print learner.evaluate_classifier()
 	cm = learner.conf_matrix()
 	print cm
+	# Show confusion matrix in a separate window
+	plt.matshow(cm)
+	plt.title('Confusion matrix')
+	plt.colorbar()
+	plt.ylabel('Actual demand')
+	plt.xlabel('Predicted demand')
+	plt.show()
