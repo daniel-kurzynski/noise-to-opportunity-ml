0a50e03e01d84f533ef52f5249552c10cb7c632c
Dimitri Korsch <korschdima@yahoo.de>
2014-11-28 18:16:40 +0100

    global variables suck

--- scikit_python/evaluation.py
+++ scikit_python/evaluation.py
@@ -5,19 +5,15 @@ from sklearn.metrics import f1_score, recall_score, \
 	confusion_matrix
 from sklearn.cross_validation import cross_val_score, train_test_split
 
-from bag_of_words import build_data
-# from custom_features import build_data
+from bag_of_words import build_data as bow
+from custom_features import build_data as custom_features
 
 from sklearn.linear_model import Perceptron
 
 def score(y_true, y_pred, score_function, label_index):
 	return score_function(y_true, y_pred, average=None)[label_index]
 
-classifier = Perceptron(n_iter = 50)
-X, y = build_data(class_names = {"demand": 0, "no-demand": 1})
-
-def evaluate_classifier():
-	global classifier
+def evaluate_classifier(classifier, X, y):
 	f1_demand_scorer = make_scorer(score, greater_is_better=True, score_function=f1_score, label_index=0)
 	f1_no_demand_scorer = make_scorer(score, greater_is_better=True, score_function=f1_score, label_index=1)
 
@@ -38,19 +34,23 @@ def evaluate_classifier():
 	print "precision-demand   ", cross_val_score(classifier, X, y, cv=5, scoring=precision_demand_scorer).mean()
 	print "precision-no-demand", cross_val_score(classifier, X, y, cv=5, scoring=precision_no_demand_scorer).mean()
 
-def conf_matrix():
+def conf_matrix(classifier, X, y):
 	X_train, X_test, y_train, y_test = train_test_split(X, y, random_state = 0)
 	y_pred = classifier.fit(X_train, y_train).predict(X_test)
 	return confusion_matrix(y_test, y_pred)
 
 if __name__ == "__main__":
-	evaluate_classifier()
-	cm = conf_matrix()
-	print cm
-	# Show confusion matrix in a separate window
-	# plt.matshow(cm)
-	# plt.title('Confusion matrix')
-	# plt.colorbar()
-	# plt.ylabel('Actual demand')
-	# plt.xlabel('Predicted demand')
-	# plt.show()
+	classifier = Perceptron(n_iter = 50)
+	for build_data in [bow, custom_features]:
+		print
+		X, y = build_data(class_names = {"demand": 0, "no-demand": 1})
+		evaluate_classifier(classifier, X, y)
+		cm = conf_matrix(classifier, X, y)
+		print cm
+		# Show confusion matrix in a separate window
+		# plt.matshow(cm)
+		# plt.title('Confusion matrix')
+		# plt.colorbar()
+		# plt.ylabel('Actual demand')
+		# plt.xlabel('Predicted demand')
+		# plt.show()
