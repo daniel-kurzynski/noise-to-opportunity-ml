44e32513101b17f6bb69a73a7d15d6998caf02c2
Dimitri Korsch <korschdima@yahoo.de>
2014-12-09 11:27:22 +0100

    added script for custom feature stat exctraction

---
+++ scikit_python/custom_feature_stats.py
@@ -0,0 +1,55 @@
+
+from os.path import dirname, join
+from numpy import array
+from collections import OrderedDict
+
+data = OrderedDict()
+data["inv-no-demand"] = None
+data["no-demand"] = None
+data["inv-demand"] = None
+data["demand"] = None
+
+def make_bin(val):
+	if float(val): return 1
+	else: return 0
+
+with open(join(dirname(dirname(__file__)), "n2o_data/features.csv")) as f:
+	first = True
+	for line in f:
+		content = line.strip().split(",")
+		if first:
+			header = content[1:]
+			header = [header[-1]] + header[:-1]
+			first = False
+			continue
+		idx, feature, cls = content[0], array([make_bin(val) for val in content[1:-1]]), content[-1]
+		if cls == "no-idea":
+			continue
+
+		cls_data = data.get(cls)
+		if cls_data is None:
+			data[cls] = feature
+		else:
+			data[cls] = cls_data + feature
+
+		cls = "inv-" + cls
+		feature_inv = array(map(lambda x: 1 if not x else 0, feature))
+		cls_data = data.get(cls)
+		if cls_data is None:
+			data[cls] = feature_inv
+		else:
+			data[cls] = cls_data + feature_inv
+
+sep = "\t"
+
+with open(join(dirname(dirname(__file__)), "features_stats.csv"), "w") as f:
+	f.write(sep.join(header))
+	f.write("\n")
+	c = 0
+	for cls, features in data.iteritems():
+		if c == 2: f.write("\n")
+		f.write(cls + sep)
+		f.write(sep.join(["%d" %(val) for val in features]))
+		f.write("\n")
+		c += 1
+
