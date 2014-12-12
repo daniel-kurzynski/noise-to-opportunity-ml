a6aaad409fcef1f67ec90e96ad6be9f44caea137
Stefan Bunk <stefan.bunk@googlemail.com>
2014-12-12 16:03:07 +0100

    Count and output important words for brochure categories.

---
+++ ml_java/src/main/scala/de/hpi/smm/domain/BrochuresCountsCounter.scala
@@ -0,0 +1,42 @@
+package de.hpi.smm.domain
+
+import scala.collection.mutable
+
+case class BrochureCounts(var crm: Int = 0, var ecom: Int = 0, var hcm: Int = 0, var lvm: Int = 0)
+
+class BrochuresCountsCounter extends mutable.HashMap[Word, BrochureCounts] {
+	val classCounts = mutable.Map[String, Int]().withDefaultValue(0)
+
+	def overallBrochures: Int = classCounts.values.sum
+
+	private def calculateRatios(): List[(Word, BrochureCounts, Double, Double, Double, Double)] = {
+		toList.map { case (word, currentCounts) =>
+			val crmProb  = currentCounts.crm.toDouble  / classCounts("CRM")
+			val ecomProb = currentCounts.ecom.toDouble / classCounts("ECOM")
+			val hcmProb  = currentCounts.hcm.toDouble  / classCounts("HCM")
+			val lvmProb  = currentCounts.lvm.toDouble  / classCounts("LVM")
+
+			(word, currentCounts, crmProb, ecomProb, hcmProb, lvmProb)
+
+//			val relation = crmProb / noDemandProb
+//
+//			val demandMissingProb   = (classCounts("demand") - currentCounts.demand).toDouble / classCounts("demand")
+//			val noDemandMissingProb = (classCounts("no-demand") - currentCounts.noDemandCount).toDouble / classCounts("no-demand")
+//			val missingRelation = noDemandMissingProb / demandMissingProb
+//			(word, currentCounts, if (relation.isInfinite) 0 else relation, if (missingRelation.isInfinite) 0 else missingRelation)
+		}
+	}
+
+	def takeCRM(i: Int): List[(Word, BrochureCounts, Double, Double, Double, Double)] = {
+		calculateRatios().sortBy(-_._3).take(i)
+	}
+	def takeECOM(i: Int): List[(Word, BrochureCounts, Double, Double, Double, Double)] = {
+		calculateRatios().sortBy(-_._4).take(i)
+	}
+	def takeHCM(i: Int): List[(Word, BrochureCounts, Double, Double, Double, Double)] = {
+		calculateRatios().sortBy(-_._5).take(i)
+	}
+	def takeLVM(i: Int): List[(Word, BrochureCounts, Double, Double, Double, Double)] = {
+		calculateRatios().sortBy(-_._6).take(i)
+	}
+}
