b4daaa099bfca1745862095904f4a8050c036881
Stefan Bunk <stefan.bunk@googlemail.com>
2014-12-12 12:54:51 +0100

    Formatting.

--- scikit_python/evaluation.py
+++ scikit_python/evaluation.py
@@ -207,10 +207,11 @@ def run_product(classifier):
 	X_train, y_train, X_test, y_true, categories = bow()
 	X_train = X_train.todense() if issparse(X_train) else X_train
 	X_test = X_test.todense() if issparse(X_test) else X_test
-	for category in categories:
 
-		y_train_category = [category if y == category else "not-"+category for y in y_train]
-		y_true_category = [category if y == category else "not-"+category for y in y_true]
+
+	for category in categories:
+		y_train_category = [category if y == category else "not-" + category for y in y_train]
+		y_true_category =  [category if y == category else "not-" + category for y in y_true]
 
 		validate(classifier, X_train, y_train_category, X_test, y_true_category)
 
