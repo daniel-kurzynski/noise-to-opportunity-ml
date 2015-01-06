d08ab3a9a205d3215810d8142d61536f632373da
Stefan Bunk <stefan.bunk@googlemail.com>
2015-01-06 14:40:10 +0100

    Nicify web view.

--- webapp_scala/src/main/webapp/script/main.js
+++ webapp_scala/src/main/webapp/script/main.js
@@ -1,15 +1,38 @@
 jQuery(function() {
 	var predictionTimer = function() {
+		var COLOR_ANIMATION = 300;
+
+		var roundValue = function(d) {
+			return Math.round(d * 100);
+		};
+
 		jQuery.getJSON("/predictions", { text: $("#blog-post").val() }, function(data) {
-			var $results = $("#results")
+			var $results = $("#results");
 			$results.empty();
 			console.log(data);
-			data.forEach(function(el) {
+			if (data.demand.prob > 0.5) {
+				$("#demand-classification").text("DEMAND").animate({
+					backgroundColor: "#43ac6a"
+				}, COLOR_ANIMATION);
+			} else {
+				$("#demand-classification").text("NO-DEMAND").animate({
+					backgroundColor: "#f04124"
+				}, COLOR_ANIMATION);
+			}
+			if (data.product[0].prob > 0.5) {
+				$("#product-classification").text(data.product[0].cls).animate({
+					backgroundColor: "#008cba"
+				}, COLOR_ANIMATION);
+			} else {
+				$("#product-classification").text("NONE").animate({
+					backgroundColor: "#5bc0de"
+				}, COLOR_ANIMATION);
+
+			}
+			data.product.forEach(function(el) {
 				// Use templates, if this gets more complicated.
-				$results.append("<tr><td><div class='result-percentage label-primary'>" +
-					el.percentage + " %</div></td><td>" + el.text + "</td></tr>")
-
-
+				$results.append("<tr><td class='product-category-label'><div class='result-percentage label-primary'>" +
+					roundValue(el.prob) + " %</div></td><td>" + el.cls + "</td></tr>")
 			});
 			window.setTimeout(predictionTimer, 2500)
 		});
