952936eb27a2cbdd6b992346f55e0b86fd7dcc2c
Daniel Kurzynski <daniel.kurzynski@student.hpi.uni-potsdam.de>
2015-01-08 23:46:18 +0100

    adapt N20Servlet.scala to use new PostClassification format

--- webapp_scala/src/main/scala/de/hpi/smm/app/N20Servlet.scala
+++ webapp_scala/src/main/scala/de/hpi/smm/app/N20Servlet.scala
@@ -1,7 +1,13 @@
 package de.hpi.smm.app
 
-import scala.util.Random
+import java.io.{File, FileReader}
 
+import com.lambdaworks.jacks.JacksMapper
+import de.hpi.smm.FeatureExtractorBuilder
+import de.hpi.smm.data_reader.DataReader
+
+import scala.util.Random
+import de.hpi.smm.classification.PostClassifier
 import org.json4s.{DefaultFormats, Formats}
 import org.scalatra.json._
 
@@ -15,6 +21,18 @@ class N20Servlet extends N20DemoStack  with JacksonJsonSupport {
 		Prediction(45, "Kostenlose Kreditkarten mit [KreditKartXYZ]")
 	)
 
+	val FOR_ALL_POSTS = false
+
+	val classifiedPosts = JacksMapper.readValue[Map[String, Map[String, Map[String, String]]]](
+		new FileReader("../webapp_python/data/classification.json"))
+	val postsFile = new File("../n2o_data/linked_in_posts.csv")
+	val brochuresFile = new File("../n2o_data/brochures.csv")
+
+	val dataReader = new DataReader(classifiedPosts, postsFile, brochuresFile, FOR_ALL_POSTS);
+
+	val featureExtractorBuilder = new FeatureExtractorBuilder(dataReader)
+	val postClassifier = new PostClassifier(featureExtractorBuilder)
+
 	get("/") {
 		contentType = "text/html"
 		jade("template")
@@ -22,10 +40,9 @@ class N20Servlet extends N20DemoStack  with JacksonJsonSupport {
 
 	val r = new Random
 	get("/predictions") {
-		val pc = new PostClassifier
 		val text = params("text")
-		val demandClassification  = pc.classifyDemand(text)
-		val productClassification = pc.classifyProduct(text)
+		val demandClassification  = postClassifier.classifyDemand(text)
+		val productClassification = postClassifier.classifyProduct(text)
 		contentType = formats("json")
 //		val before = Prediction(100, ) :: r.shuffle(DUMMIES)
 		Map(
