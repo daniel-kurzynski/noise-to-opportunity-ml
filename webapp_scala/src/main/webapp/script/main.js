a4b51a6abd5a066c3f77f46f5c7b9804e4d12f54
Stefan Bunk <stefan.bunk@googlemail.com>
2015-01-09 16:31:17 +0100

    Display demand information.

--- webapp_scala/src/main/webapp/script/main.js
+++ webapp_scala/src/main/webapp/script/main.js
@@ -22,22 +22,28 @@ jQuery(function() {
 				window.setTimeout(predictionTimer, 2500)
 			},
 			success: function (data) {
-				var $results = $("#results");
-				$results.empty();
+				var $productResults = $("#product-results");
+				var $demandResults  = $("#demand-results");
+				$productResults.empty();
+				$demandResults.empty();
 				console.log(data.demand.classificationOutput);
-				if (data.demand.classificationOutput.prob > 0.5) {
-					setTextAndAnimate("demand", "DEMAND", "#43ac6a");
+				var demandProb = data.demand.classificationOutput.prob;
+				if (demandProb > 0.5) {
+					setTextAndAnimate("demand", "DEMAND (" + roundValue(demandProb) + ")", "#43ac6a");
 				} else {
-					setTextAndAnimate("demand", "NO-DEMAND", "#f04124");
+					setTextAndAnimate("demand", "NO-DEMAND (" + roundValue(1 - demandProb) + ")", "#f04124");
 				}
 				if (data.product[0].classificationOutput.prob > 0.5) {
 					setTextAndAnimate("product", data.product[0].classificationOutput.cls, "#008cba");
 				} else {
 					setTextAndAnimate("product", "NONE", "#5bc0de");
 				}
+				data.demand.classificationOutput.relevantFeatures.forEach(function(el) {
+					$demandResults.append("<tr><td>" + el[0] + "</td><td>" + el[1] + "</td></tr>");
+				});
 				data.product.forEach(function (el) {
 					// Use templates, if this gets more complicated.
-					$results.append("<tr><td class='product-category-label'><div class='result-percentage label-primary'>" +
+					$productResults.append("<tr><td class='product-category-label'><div class='result-percentage label-primary'>" +
 						roundValue(el.classificationOutput.prob) + " %</div></td><td>" + el.cls + "</td></tr>")
 				});
 			},
