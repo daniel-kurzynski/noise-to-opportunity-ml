30d1717d3041058b540797fec8e4f65dfb25e12c
Daniel Kurzynski <daniel.kurzynski@student.hpi.uni-potsdam.de>
2015-01-16 15:52:43 +0100

    tune parameter

--- ml_java/src/main/scala/de/hpi/smm/classification/NTOAnalyzer.scala
+++ ml_java/src/main/scala/de/hpi/smm/classification/NTOAnalyzer.scala
@@ -15,22 +15,22 @@ class NTOAnalyzer(featureExtractorBuilder: FeatureExtractorBuilder) {
 
 	val CRMClassifier = new Classifier("CRM",
 		featureExtractorBuilder.brochures,
-		featureExtractorBuilder.buildForBrochures("CRM", 2.0, 5.0),
+		featureExtractorBuilder.buildForBrochures("CRM", 1.1, 6.0),
 		featureExtractorBuilder.dataReader)
 
 	val ECOMClassifier = new Classifier("ECOM",
 		featureExtractorBuilder.brochures,
-		featureExtractorBuilder.buildForBrochures("ECOM", 4.0, 100.0),
+		featureExtractorBuilder.buildForBrochures("ECOM", 1.8, 100.0),
 		featureExtractorBuilder.dataReader)
 
 	val HCMClassifier = new Classifier("HCM",
 		featureExtractorBuilder.brochures,
-		featureExtractorBuilder.buildForBrochures("HCM", 3.0, 13.0),
+		featureExtractorBuilder.buildForBrochures("HCM", 2.0, 10.0),
 		featureExtractorBuilder.dataReader)
 
 	val LVMClassifier = new Classifier("LVM",
 		featureExtractorBuilder.brochures,
-		featureExtractorBuilder.buildForBrochures("LVM", 7.0, 35.0),
+		featureExtractorBuilder.buildForBrochures("LVM", 3.4, 10.0),
 		featureExtractorBuilder.dataReader)
 
 	def classifyDemand(text: String): Classification = {
