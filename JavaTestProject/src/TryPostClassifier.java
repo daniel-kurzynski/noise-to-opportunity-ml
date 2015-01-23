7b33f261a4a2a591eb08eccf79f1e6315639fcde
Dimitri Korsch <korschdima@yahoo.de>
2015-01-23 13:37:45 +0100

    using ReadingResult class now as output of readFromCSV

--- JavaTestProject/src/TryPostClassifier.java
+++ JavaTestProject/src/TryPostClassifier.java
@@ -1,22 +1,22 @@
 import java.io.File;
-import java.util.List;
 
 import com.blog_intelligence.nto.DocumentExtractor;
-import com.blog_intelligence.nto.Document;
 import com.blog_intelligence.nto.NTOClassifier;
+import com.blog_intelligence.nto.ReadingResult;
 
 public class TryPostClassifier {
 
 	public static void main(String[] args) {
 
-		File classificationFile = new File("classification.json");
-		List<Document> posts = new DocumentExtractor().readFromCSV(new File("linked_in_posts.csv"), classificationFile, "demand");
-		List<Document> brochures = new DocumentExtractor().readFromCSV(new File("brochures.csv"), classificationFile, "category");
+		ReadingResult documents = new DocumentExtractor().readFromCSV(
+				new File("linked_in_posts.csv"),
+				new File("brochures.csv"),
+				new File("classification.json"));
 
 		NTOClassifier classifier = new NTOClassifier();
 
-		classifier.trainDemand(posts);
-		classifier.trainProduct(brochures);
+		classifier.trainDemand(documents.demandDocuments());
+		classifier.trainProduct(documents.productDocuments());
 
 		String post = "Hi! I am the CTO of Startup Inc. Lately, I have problems organising my customers. Do you have any recommendations for a good crm system to handle them?";
 
