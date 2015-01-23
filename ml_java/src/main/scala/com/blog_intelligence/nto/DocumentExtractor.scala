d66ba6afe061135998aaa95a52c8c21e659186c1
Dimitri Korsch <korschdima@yahoo.de>
2015-01-23 12:26:18 +0100

    added Document extractor class

---
+++ ml_java/src/main/scala/com/blog_intelligence/nto/DocumentExtractor.scala
@@ -0,0 +1,14 @@
+package com.blog_intelligence.nto
+
+import java.io.File
+
+class DocumentExtractor {
+
+	def readFromCSV(file: File): Unit = {
+		println("I am doing nothing.")
+	}
+
+	def readFromDB(host: String, port: Int, username: String, pwd: String): Unit = {
+		println("I am doing nothing.")
+	}
+}
