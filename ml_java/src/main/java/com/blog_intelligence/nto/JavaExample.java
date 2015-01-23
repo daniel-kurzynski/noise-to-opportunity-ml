cb1097cf2edb0128fde04144996f1849cbc1e826
Dimitri Korsch <korschdima@yahoo.de>
2015-01-23 14:51:33 +0100

    created example in java for ml_java project

---
+++ ml_java/src/main/java/com/blog_intelligence/nto/JavaExample.java
@@ -0,0 +1,25 @@
+package com.blog_intelligence.nto;
+
+import java.io.File;
+
+public class JavaExample {
+
+	public static void main(String[] args) {
+
+		ReadingResult documents = new DocumentExtractor().readFromCSV(
+				new File("../n2o_data/linked_in_posts.csv"),
+				new File("../n2o_data/brochures.csv"),
+				new File("../n2o_data/classification_updates/latest.json"));
+
+		NTOClassifier classifier = new NTOClassifier();
+
+		classifier.trainDemand(documents.demandDocuments());
+		classifier.trainProduct(documents.productDocuments());
+
+		String post = "Hi! I am the CTO of Startup Inc. Lately, I have problems organising my customers. Do you have any recommendations for a good crm system to handle them?";
+		double value = classifier.predictDemand(post);
+
+		System.out.println(value);
+
+	}
+}
