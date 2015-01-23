4f223b6c5d3871f90aebf4dd7e9f9322ea1a44ee
Dimitri Korsch <korschdima@yahoo.de>
2015-01-23 12:07:54 +0100

    ignored lib folder. modified jar creation script

--- webapp_scala/build-and-copy-jar.sh
+++ webapp_scala/build-and-copy-jar.sh
@@ -3,7 +3,8 @@
 set -e
 
 cd ../ml_java && mvn package -DskipTests
-mv target/nto-0.1-SNAPSHOT.jar ../webapp_scala/lib/nto-0.1-SNAPSHOT.jar
+cp target/nto-0.1-SNAPSHOT.jar ../webapp_scala/lib/nto-0.1-SNAPSHOT.jar
+cp target/nto-0.1-SNAPSHOT.jar ../JavaTestProject/lib/nto-0.1-SNAPSHOT.jar
 cd ../webapp_scala
 sbt
 
