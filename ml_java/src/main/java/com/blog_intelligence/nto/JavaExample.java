0edfb30086c14ba8e117fd972dd271a0f6806be1
Dimitri Korsch <korschdima@yahoo.de>
2015-01-23 14:53:32 +0100

    pasted stefans changes

--- ml_java/src/main/java/com/blog_intelligence/nto/JavaExample.java
+++ ml_java/src/main/java/com/blog_intelligence/nto/JavaExample.java
@@ -1,25 +1,44 @@
 package com.blog_intelligence.nto;
 
 import java.io.File;
+import java.util.List;
 
 public class JavaExample {
 
-	public static void main(String[] args) {
+	static DataBaseConfiguration CONFIG = new DataBaseConfiguration("141.89.225.134","30315","SMA1415", "Popcorn54","SMA1415.CLASSIFIED_POSTS");
 
-		ReadingResult documents = new DocumentExtractor().readFromCSV(
+	public static void main(String[] args) {
+		/**
+		 * Reading training data
+		 */
+		DocumentExtractor documentExtractor = new DocumentExtractor();
+
+		ReadingResult documents = documentExtractor.readFromCSV(
 				new File("../n2o_data/linked_in_posts.csv"),
 				new File("../n2o_data/brochures.csv"),
 				new File("../n2o_data/classification_updates/latest.json"));
 
+		ReadingResult dbDocs = documentExtractor.readFromDB(CONFIG);
+
+		/**
+		 * Building classifier
+		 */
 		NTOClassifier classifier = new NTOClassifier();
 
 		classifier.trainDemand(documents.demandDocuments());
 		classifier.trainProduct(documents.productDocuments());
 
+		/**
+		 * Predicting demand and product
+		 */
 		String post = "Hi! I am the CTO of Startup Inc. Lately, I have problems organising my customers. Do you have any recommendations for a good crm system to handle them?";
-		double value = classifier.predictDemand(post);
 
-		System.out.println(value);
+		double probDemand = classifier.predictDemand(post);
+		System.out.println("Demand probability " + probDemand);
 
+		List<NTOClassifier.Classification> probsProduct = classifier.predictProduct(post);
+		for (NTOClassifier.Classification classification : probsProduct) {
+			System.out.println(classification.product() + ": " + classification.prob());
+		}
 	}
 }
